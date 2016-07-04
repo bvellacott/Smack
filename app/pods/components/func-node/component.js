d25ce57807645520ab6be400eaceccafbaa502c8
Benjamin Vellacott <bmvellacott@gmail.com>
2016-07-04 23:40:26 +0100

    added messagin suppoert and made tree menu utilise it

--- app/pods/components/func-node/component.js
+++ app/pods/components/func-node/component.js
@@ -1,21 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+
 	tagName: 'li',
 	classNames: [ 'leaf' ],
 	unit: { name: 'moi!' },
 
+	rootName: 'something',
 	parentPath: null,
 	treePath : Ember.computed('parentPath', 'name', function() {
     return (this.parentPath ? this.parentPath + '.' : '') + this.name;
   }),
-	selected : Ember.computed('selectedPath', 'treePath', function() {
-    return this.selectedPath && this.selectedPath === this.treePath;
+	selected : Ember.computed('selectedId', function() {
+    return this.selectedId && this.selectedId === this.elementId;
   }),
   actions: {
-  	toggleSelected(path) {
-	  	if(this.name === this.get('treePath'))
-	  		this.set('selectedPath', path);
+  	select() {
+	  	Ember.Messaging.notify(this.rootName + '.item.select', this.elementId);
   	}
-  }
+  },
+  init() {
+    this._super(...arguments);
+    Ember.Messaging.setListener(this, this.rootName + '.item.select', function(elementId){
+    	this.set('selectedId', elementId);
+    });
+  },
+	willDestroyElement() {
+	  this._super(...arguments);
+    Ember.Messaging.removeListener(this, this.rootName + '.item.select');
+	}
 });
--- vendor/general.js
+++ vendor/general.js
@@ -2,11 +2,35 @@ define('general', ['exports', 'ember'], function (exports, _ember) {
   'use strict';
 
   var ember = _ember['default'];
+  var $ = ember.$;
 
   ember.Messaging = {
   	context: {},
-  	addListener: function(name, eventName, func) {
-
+  	getListeners: function(eventName) {
+  		var listeners = this.context[eventName];
+  		if(!listeners)
+  			listeners = this.context[eventName] = {};
+  		return listeners;
+  	},
+  	setListener: function(obj, eventName, func) {
+  		this.getListeners(eventName)[obj.elementId] = { obj: obj, func: func };
+  	},
+  	getListener: function(obj, eventName) {
+  		return this.getListeners(eventName)[obj.elementId];
+  	},
+  	removeListener: function(obj, eventName) {
+  		var listeners = this.context[eventName];
+  		if(listeners)
+  			delete listeners[obj.elementId];
+   	},
+  	notify: function() {
+  		var args = $.makeArray(arguments);
+  		var eventName = args.splice(0, 1);
+  		var listeners = this.getListeners(eventName);
+  		for(var name in listeners) {
+  			var pair = listeners[name];
+  			pair.func.apply(pair.obj, args);
+  		}
   	}
   };
 
@@ -27,7 +51,8 @@ define('general', ['exports', 'ember'], function (exports, _ember) {
 				cur = cur[packs[i]];
 			}
 			cur[unit.get('name')] = unit;
-		}
+		},
+
 
 	// 	toPackageTree: function(units) {
 	// 		var tree = ember.Object.create();
