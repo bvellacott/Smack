c94ce90c8ee0a92b25659e9a3733a428a074a253
Benjamin Vellacott <bmvellacott@gmail.com>
2016-07-15 00:40:01 +0100

    removed obsolete list-menu component

--- app/pods/components/list-menu/component.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-	tagName: 'ul',
-	classNames: ['list-menu'],
-  classNameBindings: [ 'expanded:expanded:collapsed' ],
-  // expanded: true,
-});
